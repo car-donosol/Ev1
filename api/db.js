b369d4a180420f1c85e89b945c9fe2dcdb36fe94
evairx <xxbrazzersxx35@gmail.com>
2025-09-11 05:09:36 -0300

    mejore estilos del menu, y dettale.js

--- api/db.js
+++ api/db.js
@@ -1,12 +1,12 @@
 export const data = [
     {
         id: 1,
-        name: "𝗖𝗨𝗡𝗔 𝗗𝗘 𝗠𝗢𝗜𝗦É𝗦",
+        name: "CUNA DE MOISES",
         image: "496936885.jpg"
     },
         {
         id: 2,
-        name: "𝗙𝗜𝗖𝗨𝗦 𝗘𝗟Á𝗦𝗧𝗜𝗖𝗔 ‘𝗡𝗘𝗚𝗥𝗢’ ",
+        name: "FICUS ELASTICA NEGRO",
         image: "494994013.jpg"
     }
 ]
--- assets/js/detalle.js
+++ assets/js/detalle.js
@@ -8,9 +8,26 @@ if(!id) window.location.href = "404.html"
 
 const res = data.find(s => s.id == id)
 
-if(!res) window.location.href = "404.html"
-
 const div = document.getElementById("detalle")
+
+if(!res) {
+    document.title = "Producto no encontrado - Huertabeja";
+    const section = document.createElement("section")
+    section.className = "container-404"
+
+    const h4 = document.createElement("h4")
+    h4.textContent = "404 Producto no encontrado"
+
+    const p = document.createElement("p")
+    p.textContent = "El producto que ha solicitado no existe"
+
+    section.appendChild(h4)
+    section.appendChild(p)
+
+    div.appendChild(section)
+}
+
+document.title = `${res.name} - Huertabeja`;
 
 const imagen = document.createElement("img")
 imagen.src = `./assets/img/${res.image}`
