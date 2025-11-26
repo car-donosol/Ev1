26b052ba6f401ebf3b3210754a379928bdf3daff
car-donosol <car.donosol@duocuc.cl>
2025-11-26 09:08:25 -0300

    página de inicio a home

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,14 @@
 import { Suspense } from "react";
 import Productos from "@/components/server/productos";
-import HomePage from "@/components/server/productos";
+
+import HomePage from "@/app/(main)/home/page";
 
 
 export default function Home() {
   return (
     <div className="desktop-md:px-6">
       <Suspense fallback={<div>Cargando productos...</div>}>
-        <Productos />
+        <HomePage />
       </Suspense>
     </div>
   );
